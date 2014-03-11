16a4d9ecf620ecd6b146ad353875d62173f02c38
Tim Webster <tim.webster@gmail.com>
2014-03-11 22:15:07 +0000

    tidy up pom

--- src/main/java/org/sonar/plugins/issueassign/Users.java
+++ src/main/java/org/sonar/plugins/issueassign/Users.java
@@ -19,7 +19,7 @@
  */
 package org.sonar.plugins.issueassign;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.user.User;
--- src/main/java/org/sonar/plugins/issueassign/util/PluginUtils.java
+++ src/main/java/org/sonar/plugins/issueassign/util/PluginUtils.java
@@ -19,7 +19,7 @@
  */
 package org.sonar.plugins.issueassign.util;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.config.Settings;
